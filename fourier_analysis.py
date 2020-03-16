ea95dac9bad4ca5b8ea51a465622e072049f1d15
Thomas Steindl <thomas.steindl@student.uibk.ac.at>
2020-03-16 15:20:42 +0100

    finish iteration

--- fourier_analysis.py
+++ fourier_analysis.py
@@ -62,32 +62,52 @@ def load_results(path: str):
                                    'phase': ufloat_fromstr})
     return settings, statistics, results
 
-def get_freqs_to_remove():
-    settings, statistics, results = load_results('removed_binary_first_time/data/result.csv')
+def get_freqs_to_remove(file):
+    print(file + 'data/results.csv')
+    settings, statistics, results = load_results(file + 'data/result.csv')
     fs = results['frequency'].values
     amps = results['amp'].values
     snrs = results['snr'].values
     phis = results['phase'].values
-    freqs = []
+    freqs_init = []
     for i in range(len(fs)):
         if snrs[i]>3.8:
-            freqs.append(mode(fs[i].n, amps[i].n, phis[i].n, snrs[i]))
+            freqs_init.append(mode(fs[i].n, amps[i].n, phis[i].n, snrs[i]))
 
-    fig, ax = plt.subplots(2,1, figsize = (12,8))
-    freqs_to_remove = []
-    fs = []
-    for f in freqs:
-        #fs.append(f.f)
-        if  (0.025 > f.f % (0.5988495842998753) and f.f < 10)  or  (f.f % (0.5988495842998753)  >(0.5988495842998753)-0.025 and f.f <10) or f.f < 0.25 :
-            ax[0].plot([f.f, f.f], [0, f.snr], 'k--')
+    freqs = []
+    freqs_rl = []
+    for i in range(1, len(freqs_init)):
+        fnew = freqs_init[i]
+        new = True
+        if fnew.f < 0.25:
+            new = False
+        for f in freqs_init[:i]:
+            if abs(fnew.f-f.f) < 0.01:
+                new = False
+        if new:
+            freqs.append(fnew)
         else:
-            fs.append(f.f)
+            freqs_rl.append(fnew)
+    freqs_binary = []
+    freqs_to_remove = []
+    for f in freqs:
+        if  (0.025 > f.f % (0.5988495842998753) and f.f < 10)  or  (f.f % (0.5988495842998753)  >(0.5988495842998753)-0.025 and f.f < 10):
+            freqs_binary.append(f)
+        else :
             freqs_to_remove.append(f)
-            ax[0].plot([f.f, f.f], [0, f.snr], 'k-')
-    for i in range(1,17):
-        ax[0].plot([i*0.5988495842998753, i*0.5988495842998753], [0, 5], 'r--')
-    #for i in range(1,60):
-    #    ax.plot([i*1/1.6699, i*1/1.6699], [0, 2.5], 'b--')
+
+    fig, ax = plt.subplots(2,1, figsize=(15,8))
+    for i in range(1,25):
+        ax[0].plot([i*0.5988495842998753, i*0.5988495842998753], [0, 0.001], 'r--')
+    for f in freqs_binary:
+        ax[0].plot([f.f, f.f], [0, f.amp], 'k--')
+    for f in freqs_rl:
+        ax[0].plot([f.f, f.f], [0, f.amp], 'k:')
+    fs = []
+    for f in freqs_to_remove:
+        ax[0].plot([f.f, f.f], [0, f.amp], 'k-')
+        fs.append(f.f)
+
 
     ax[0].set_xlabel('Frequency ($d^{-1}$')
     ax[0].set_ylabel('SNR')
@@ -102,7 +122,7 @@ def get_freqs_to_remove():
 
 
     plt.show()
-    return freqs_to_remove
+    return freqs_to_remove, freqs_binary, freqs_rl
 
 
 def sin(x: np.ndarray, amp: float, f: float, phase: float) -> np.ndarray:
@@ -136,25 +156,49 @@ def sin_multiple(x: np.ndarray, *params) -> np.ndarray:
 
     return y
 
-ftr = get_freqs_to_remove()
-time, flux = load_lc('removed_binary_first_time.txt', plot = False, parts_of_lc=False)
-print(time, flux)
-mag = np.zeros(len(time))
-for f in ftr:
-    mag = mag + fourier_model(time, [f.f], [f.amp], [f.phase])
 
-fig, ax = plt.subplots(2,1, figsize = (12,8))
-ax[0].plot(time, flux, 'ko', markersize = 0.75)
-ax[0].plot(time, mag, 'r-')
-ax[1].plot(time, flux-mag, 'ko', markersize = 0.75)
-plt.show()
+def remove_pulsations(starting_lc, smurfs_result, residuals_lc, output_dir):
+    orig_lc = np.loadtxt(starting_lc).T
 
 
-fig, ax = plt.subplots( figsize = (12,8))
-time, flux = load_lc('RS_Cha_lightcurve.txt', plot = False, parts_of_lc=False)
-ax.plot(time, flux, 'ko', markersize = 0.75)
-ax.plot(time, flux - mag, 'r-')
-plt.show()
+    ftr, fb, frl = get_freqs_to_remove(smurfs_result)
+    time, flux = load_lc(residuals_lc, plot = False, parts_of_lc=False)
+    flux_mod = np.zeros(len(time))
+    for f in ftr:
+        flux_mod = flux_mod + fourier_model(time, [f.f], [f.amp], [f.phase])
+    #for f in fb:
+    #    flux_mod = flux_mod + fourier_model(time, [f.f], [f.amp], [f.phase])
+    for f in frl:
+        flux_mod = flux_mod + fourier_model(time, [f.f], [f.amp], [f.phase])
+    flux_mod = flux_mod + fourier_model(time, [11.0704029], [0.001677], [0.3697])
 
+    fig3 = plt.figure(constrained_layout=True, figsize=(15,8))
+    gs = fig3.add_gridspec(3, 3)
+    ax00 = fig3.add_subplot(gs[0, :-1])
+    ax01 = fig3.add_subplot(gs[1, :-1])
+    ax02 = fig3.add_subplot(gs[2, :-1])
+    ax10 = fig3.add_subplot(gs[0, -1:])
+    ax11 = fig3.add_subplot(gs[1, -1:])
+    ax12 = fig3.add_subplot(gs[2, -1:])
+    ax00.plot(time, flux, 'ko', markersize = 0.75)
+    ax10.plot(time, flux, 'ko', markersize = 0.75)
+    ax00.plot(time, flux_mod, 'r-')
+    ax10.plot(time, flux_mod, 'r-')
+    for ax in [ax00, ax01, ax10, ax11, ax02, ax12]:
+        ax.set_xlabel('Time - 2457000 [BTJD days]')
+        ax.set_ylabel('normalized flux')
+    for ax in [ax10, ax11, ax12]:
+        ax.set_xlim(np.min(time), np.min(time) + 3.2)
+    ax01.plot(time, flux-flux_mod, 'ko', markersize = 0.75)
+    ax11.plot(time, flux-flux_mod, 'ko', markersize = 0.75)
+    ax02.plot(time, orig_lc[1], 'ko', markersize = 0.75)
+    ax12.plot(time, orig_lc[1], 'ko', markersize = 0.75)
+    ax02.plot(time, orig_lc[1]+flux_mod, 'ro', markersize = 0.5)
+    ax12.plot(time, orig_lc[1]+flux_mod, 'ro', markersize = 0.5)
+    plt.tight_layout()
+    #plt.show()
+    np.savetxt(f'endurance/{output_dir}/removed_pulsation.txt', np.array([time, flux-flux_mod,]).T)
+    np.savetxt(f'endurance/{output_dir}/result_{output_dir}.txt', np.array([time, orig_lc[1]+flux_mod]).T)
+    plt.savefig(f'endurance/{output_dir}/result_{output_dir}.png')
 
-np.savetxt('LC_iteration_1.txt', np.array([time, flux - mag]).T)
+remove_pulsations('endurance/RS_Cha_lightcurve.txt', 'endurance/iteration1/residuals/', 'endurance/iteration1/residuals.txt', output_dir='iteration1')
