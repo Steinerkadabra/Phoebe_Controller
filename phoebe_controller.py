76f7d020a52a59386adb5b67080fc70b794c1919
Thomas Steindl <thomas.steindl@student.uibk.ac.at>
2020-03-30 10:26:22 +0200

    Update

--- phoebe_controller.py
+++ phoebe_controller.py
@@ -174,8 +174,8 @@ def chi_square_multi(input_vals, flux, times, sigmas, exp_time = False):
         #binary['exptime'] = 2, 's'
         #binary.run_compute(fti_method='oversample',  model = 'mod', overwrite= True)
         ### grav_bol####
-        binary['gravb_bol@primary'] = 0.5
-        binary['gravb_bol@secondary'] = 0.5
+        binary['gravb_bol@primary'] = 0.8
+        binary['gravb_bol@secondary'] = 0.8
         binary.run_compute(irrad_method='horvat', model='mod', overwrite=True)
         #### irrad method
         #binary['irrad_frac_refl_bol@primary'] = 0.9
